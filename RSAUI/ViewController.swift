1744a8a9672215b7f1b8057cf86ca9942c076359
yuqiang <1054572107@qq.com>
2018-08-09 11:56:50 +0800

    Initial Commit

---
+++ RSAUI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RSAUI
+//
+//  Created by sungrow on 2018/8/9.
+//  Copyright © 2018年 sungrow. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
